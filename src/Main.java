5aaebc7601020f58c59f3bd5b826f582285f7857
Dmitry Boychevsky <javashdrug@yandex.ru>
2023-03-07 22:51:04 +0300

    correction2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int availableAmount = 500; // начальная сумма на счёте//
-        int amountForBonuses = 1000; // сумма для начисления бонусов//
-        int replenishment = 1600; // сумма пополнения//
-        int bonus = replenishment / 100; // расчёт бонуса//
+        int availableAmount = 500;
+        int amountForBonuses = 1000;
+        int replenishment = 1600;
+        int bonus = replenishment / 100;
 
         if (replenishment > amountForBonuses) {
             bonus = replenishment / 100;
+        } else {
+            bonus = 0;
         }
-        System.out.println(availableAmount + replenishment + bonus); // итоговая сумма
-        System.out.println(bonus); // бонусы
+        System.out.println(availableAmount + replenishment + bonus);
+        System.out.println(bonus);
     }
+
 }
+
+
